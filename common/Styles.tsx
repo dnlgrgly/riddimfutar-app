71b85dfaa2e910fe5ea7e3d9ca035f5ae24db94d
dnlgrgly <dnlgrgly@icloud.com>
2022-06-04 00:43:24 +0200

    Add fonts, init Styles

---
+++ common/Styles.tsx
@@ -0,0 +1,14 @@
+export const Fonts = {
+  ui: "OpenSans-Regular",
+  uiBold: "OpenSans-Bold",
+  uiItalic: "OpenSans-Italic",
+  rolling: "5by7",
+  brand: "RoadRage",
+};
+
+export const Colors = {
+  yellow: "",
+  purple: "",
+  black: "",
+  white: "",
+};
--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,16 +1,57 @@
 import Geolocation from "@react-native-community/geolocation";
 import axios from "axios";
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { forceTouchGestureHandlerProps } from "react-native-gesture-handler/lib/typescript/handlers/ForceTouchGestureHandler";
+import { Fonts } from "../common/Styles";
 
 export const HomePage = () => {
+  const [announcement, setAnnouncement] = useState("");
+
   useEffect(() => {
     Geolocation.getCurrentPosition((info) => console.log(info));
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>hello!</Text>
+      <Text
+        style={{
+          fontFamily: Fonts.rolling,
+          fontWeight: "bold",
+          color: "white",
+          fontSize: 30,
+        }}
+      >
+        Hello!
+      </Text>
+      <Text
+        style={{
+          fontFamily: Fonts.brand,
+          fontWeight: "bold",
+          color: "white",
+          fontSize: 30,
+        }}
+      >
+        Hello!
+      </Text>
+      <Text
+        style={{
+          fontFamily: Fonts.uiBold,
+          color: "white",
+          fontSize: 30,
+        }}
+      >
+        Hello!
+      </Text>
+      <Text style={{ fontFamily: Fonts.ui, color: "white", fontSize: 30 }}>
+        Hello!
+      </Text>
+      <Text
+        style={{ fontFamily: Fonts.uiItalic, color: "white", fontSize: 30 }}
+      >
+        Hello!
+      </Text>
     </View>
   );
 };
@@ -19,6 +60,7 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
-    backgroundColor: "#00aaaa",
+    backgroundColor: "black",
+    color: "white",
   },
 });
