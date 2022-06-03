3e89edf7de3d5ce773d8c6f342cefa4e2116890a
dnlgrgly <dnlgrgly@icloud.com>
2022-06-04 00:30:29 +0200

    initialize typescript, geolocation, appicon

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Geolocation from "@react-native-community/geolocation";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const HomePage = () => {
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) => console.log(info));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>hello!</Text>
